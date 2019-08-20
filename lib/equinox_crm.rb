7ce2a36cb56bf4e313c58335aeae279e23d86fc7
radv.ss.dev1 <radv.ss.dev1@gmail.com>
2019-08-20 10:55:17 +0530

    file loading issues

--- lib/equinox_crm.rb
+++ lib/equinox_crm.rb
@@ -1,6 +1,6 @@
-require_relative "equinox_crm/version"
-require_relative "equinox_crm/contact"
-require_relative "equinox_crm/client"
+require "equinox_crm/version"
+require "equinox_crm/contact"
+require "equinox_crm/client"
 
 module EquinoxCrm
   class Error < StandardError; end
